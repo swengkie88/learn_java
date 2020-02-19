ae0877f154a345a244a98c700136a7ed4a95da26
swengkie88 <angkiprana1987@gmail.com>
2020-02-19 20:08:52 +0700

    Update ganjil genap

--- GanjilGenap.java
+++ GanjilGenap.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 class GanjilGenap{
+    static int bilangan;
     
+    static int apakahGanjil(int bilangan){
+        if(bilangan % 2 == 1){
+            System.out.println("Bilangan anda = " + bilangan + " Nilainya ganjil");
+        }else{
+            System.out.println("Bilangan anda = " + bilangan + " Nilainya genap");
+        }
+        return bilangan;
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan bilangganya...");
+        apakahGanjil(input.nextInt());
+    }
 }
